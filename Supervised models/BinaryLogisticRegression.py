75241f2b4cc2ace87f14145b4a6bcd31b33536ef
Jens Norell <jens.norell@tretton37.com>
2022-01-25 15:09:27 +0100

    Added Mean-Shit & Binary Logistic Regression algorithms

---
+++ Supervised models/BinaryLogisticRegression.py
@@ -0,0 +1,58 @@
+# %%
+
+# Example from https://realpython.com/logistic-regression-python/
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+
+# %%
+
+# Generate random data
+
+x = np.arange(10).reshape(-1, 1)
+y = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+
+# %%
+
+# Create model and train it
+
+model = LogisticRegression(solver='liblinear', random_state=0, penalty='l2') # penalty parameter decides which regularization is applied
+model.fit(x, y)
+
+# %%
+
+# Evaluate model
+
+model.predict_proba(x)
+model.predict(x)
+model.score(x, y)
+
+# Confusion matrix, shows the numbers of the following:
+
+# True negatives in the upper-left position
+# False negatives in the lower-left position
+# False positives in the upper-right position
+# True positives in the lower-right position
+
+cm = confusion_matrix(y, model.predict(x))
+
+# Visualize it
+
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.imshow(cm)
+ax.grid(False)
+ax.xaxis.set(ticks=(0, 1), ticklabels=('Predicted 0s', 'Predicted 1s'))
+ax.yaxis.set(ticks=(0, 1), ticklabels=('Actual 0s', 'Actual 1s'))
+ax.set_ylim(1.5, -0.5)
+for i in range(2):
+    for j in range(2):
+        ax.text(j, i, cm[i, j], ha='center', va='center', color='red')
+plt.show()
+
+# If you want to improve the model accuracy, go back and use model = LogisticRegression() with different inputs,such as another solver or different regularization
+
+
+
+
---
+++ Unsupervised models/Mean-Shit Clustering.py
@@ -0,0 +1,49 @@
+# %% Import
+
+# Example from https://analyticsindiamag.com/hands-on-tutorial-on-mean-shift-clustering-algorithm/
+
+import numpy as np
+from sklearn.cluster import MeanShift
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+from sklearn.cluster import estimate_bandwidth
+
+
+# %% Set up clusters and visualize
+
+# Set up coordinates that the generated data will revolve around
+coordinates = [[2, 2, 3], [6, 7, 8], [5, 10, 13]]
+
+X, _ = make_blobs(n_samples=120, centers=coordinates, cluster_std=0.60)
+
+data_fig = plt.figure(figsize=(12, 10))
+ax = data_fig.add_subplot(111, projection='3d')
+ax.scatter(X[:, 0], X[:, 1], X[:, 2], marker='o', color='green')
+plt.show()
+
+# %% Implement Mean-Shift to predict the cluster and define the centroids of the clusters.
+
+# Estimate bandwidth parameter
+
+bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
+
+msc = MeanShift(bandwidth=bandwidth, bin_seeding=True).fit(X)
+cluster_centers = msc.cluster_centers_
+labels = msc.labels_
+cluster_label = np.unique(labels)
+n_clusters = len(cluster_label)
+# n_clusters = 3, as it should be
+
+# %% Visualize the clusters
+
+msc_fig = plt.figure(figsize=(12, 10))
+
+ax = msc_fig.add_subplot(111, projection='3d')
+
+ax.scatter(X[:, 0], X[:, 1], X[:, 2], marker='o', color='yellow')
+
+ax.scatter(cluster_centers[:, 0], cluster_centers[:, 1],
+           cluster_centers[:, 2], marker='o', color='green',
+           s=300, linewidth=5, zorder=10)
+plt.title('Estimated number of clusters: %d' % n_clusters)
+plt.show()
